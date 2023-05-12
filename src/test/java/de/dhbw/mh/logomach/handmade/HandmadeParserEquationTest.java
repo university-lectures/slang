18919a250fe044ab2a1413e6edd996563f43d728
marco-haupt <132950989+marco-haupt@users.noreply.github.com>
2023-05-13 00:49:51 +0200

    !feat: code location converter added to simplify tests

--- src/test/java/de/dhbw/mh/logomach/handmade/HandmadeParserEquationTest.java
+++ src/test/java/de/dhbw/mh/logomach/handmade/HandmadeParserEquationTest.java
@@ -10,7 +10,9 @@ import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mockito;
@@ -27,19 +29,19 @@ class HandmadeParserEquationTest extends HandmadeParserUtils {
 	
 	private static Stream<Arguments> delegation_errorDetection() {
 		return Stream.of(
-				Arguments.of( "+",  new CodeLocation(0,1,1) ),
-				Arguments.of( "-",  new CodeLocation(0,1,1) ),
-				Arguments.of( "*",  new CodeLocation(0,1,1) ),
-				Arguments.of( "/",  new CodeLocation(0,1,1) ),
-				Arguments.of( "%",  new CodeLocation(0,1,1) ),
-				Arguments.of( "**", new CodeLocation(0,2,2) ),
-				Arguments.of( "(",  new CodeLocation(0,1,1) ),
-				Arguments.of( "<",  new CodeLocation(0,1,1) ),
-				Arguments.of( ">",  new CodeLocation(0,1,1) ),
-				Arguments.of( "<=", new CodeLocation(0,2,2) ),
-				Arguments.of( ">=", new CodeLocation(0,2,2) ),
-				Arguments.of( "12", new CodeLocation(0,2,2) ),
-				Arguments.of( "xy", new CodeLocation(0,2,2) )
+				Arguments.of( "+",  "0:1:1" ),
+				Arguments.of( "-",  "0:1:1" ),
+				Arguments.of( "*",  "0:1:1" ),
+				Arguments.of( "/",  "0:1:1" ),
+				Arguments.of( "%",  "0:1:1" ),
+				Arguments.of( "**", "0:2:2" ),
+				Arguments.of( "(",  "0:1:1" ),
+				Arguments.of( "<",  "0:1:1" ),
+				Arguments.of( ">",  "0:1:1" ),
+				Arguments.of( "<=", "0:2:2" ),
+				Arguments.of( ">=", "0:2:2" ),
+				Arguments.of( "12", "0:2:2" ),
+				Arguments.of( "xy", "0:2:2" )
 		);
 	}
 	
@@ -134,7 +136,9 @@ class HandmadeParserEquationTest extends HandmadeParserUtils {
 
 	@ParameterizedTest
 	@MethodSource("delegation_errorDetection")
-	void equalities_detectsSyntaxErrors( String input, CodeLocation location ){
+	void equalities_detectsSyntaxErrors(
+			String input,
+			@ConvertWith(CodeLocationConverter.class) CodeLocation location ){
 		AstNode previousSubtree = DUMMY_LITERAL1;
 		initializeParser( input );
 		
@@ -144,9 +148,12 @@ class HandmadeParserEquationTest extends HandmadeParserUtils {
 		.withMessageContainingAll( "'=='", "'!='", "'&&'", "'||'", "<EOF>", "')'" );
 	}
 	
-	@ParameterizedTest
+	@ParameterizedTest( name = "mismatched input {0} at 0:0:0-{1}, expected ==" )
 	@MethodSource("delegation_errorDetection")
-	void equalities1_rejectsOtherThan_EQUAL( String input, CodeLocation location ){
+	@CsvSource({ "!=,0:2:2" })
+	void equalities1_rejectsOtherThan_EQUAL(
+			String input,
+			@ConvertWith(CodeLocationConverter.class) CodeLocation location ){
 		AstNode previousSubtree = DUMMY_LITERAL1;
 		initializeParser( input );
 		
@@ -156,9 +163,12 @@ class HandmadeParserEquationTest extends HandmadeParserUtils {
 		.withMessageContainingAll( "'=='" );
 	}
 	
-	@ParameterizedTest
+	@ParameterizedTest( name = "mismatched input {0} at 0:0:0-{1}, expected !=" )
 	@MethodSource("delegation_errorDetection")
-	void equalities2_rejectsOtherThan_UNEQUAL( String input, CodeLocation location ){
+	@CsvSource({ "==,0:2:2" })
+	void equalities2_rejectsOtherThan_UNEQUAL(
+			String input,
+			@ConvertWith(CodeLocationConverter.class) CodeLocation location ){
 		AstNode previousSubtree = DUMMY_LITERAL1;
 		initializeParser( input );
 		
