9f30d97413538f1d887c4d5a92862e8a2ee624f5
I3ene <benedikt.muell@t-online.de>
2023-05-16 12:01:50 +0200

    first approach

--- src/main/java/de/dhbw/mh/slang/craft/parser/CraftedSlangParser.java
+++ src/main/java/de/dhbw/mh/slang/craft/parser/CraftedSlangParser.java
@@ -29,8 +29,10 @@ import java.util.stream.Stream;
 
 import de.dhbw.mh.slang.Bool;
 import de.dhbw.mh.slang.NumericValue;
+import de.dhbw.mh.slang.ast.AstBinaryOperation;
 import de.dhbw.mh.slang.ast.AstLiteral;
 import de.dhbw.mh.slang.ast.AstNode;
+import de.dhbw.mh.slang.ast.AstBinaryOperation.Operator;
 import de.dhbw.mh.slang.craft.CodeLocation;
 import de.dhbw.mh.slang.craft.Token;
 import de.dhbw.mh.slang.craft.lexer.CraftedSlangLexer;
@@ -232,32 +234,53 @@ public class CraftedSlangParser extends AbstractParserLL1 {
 	
 	@Override
 	public AstNode additiveExpression( ){
-		// TODO Auto-generated method stub
-		return super.additiveExpression( );
+		if( Selector.SUMMAND.contains(LEXER.lookahead().TYPE) ){
+			AstNode previous = atomicExpression();
+			return summand( previous );
+		}
+		throw parsingException( Selector.SUMMAND );
 	}
 	
 	@Override
 	public AstNode summand( AstNode previous ){
-		// TODO Auto-generated method stub
-		return super.summand( previous );
+		Token token = LEXER.lookahead();
+		if( Selector.SUMMAND3.contains(token.TYPE) ){
+			return summand3( previous );
+		}
+		if( PLUS == token.TYPE ){
+			return summand1( previous );
+		}
+		if( MINUS == token.TYPE ){
+			return summand2( previous );
+		}
+		throw parsingException( Selector.SUMMAND );
 	}
 	
 	@Override
 	AstNode summand1( AstNode previous ){
-		// TODO Auto-generated method stub
-		return super.summand1( previous );
+		if (PLUS == LEXER.lookahead().TYPE) {
+			CodeLocation location = LEXER.lookahead().BEGIN;
+			LEXER.advance();
+			AstNode next = additiveExpression();
+			return new AstBinaryOperation( location, previous, Operator.ADD, next);
+		}
+		throw parsingException( Selector.SUMMAND );
 	}
 	
 	@Override
 	AstNode summand2( AstNode previous ){
-		// TODO Auto-generated method stub
-		return super.summand2( previous );
+		if (MINUS == LEXER.lookahead().TYPE) {
+			CodeLocation location = LEXER.lookahead().BEGIN;
+			LEXER.advance();
+			AstNode next = additiveExpression();
+			return new AstBinaryOperation( location, previous, Operator.SUBTRACT, next );
+		}
+		throw parsingException( Selector.SUMMAND );
 	}
 	
 	@Override
 	AstNode summand3( AstNode previous ){
-		// TODO Auto-generated method stub
-		return super.summand3( previous );
+		return previous;
 	}
 	
 	/*===========================================================
