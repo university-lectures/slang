2106064d42de2ad46925d9b667291f7f49a2ab1d
Claiyc <claiyc@outlook.com>
2023-05-26 14:20:24 +0200

    enable test

--- src/test/java/de/dhbw/mh/slang/codegen/JavaBytecodeGenerator8Test.java
+++ src/test/java/de/dhbw/mh/slang/codegen/JavaBytecodeGenerator8Test.java
@@ -10,17 +10,16 @@ import org.junit.jupiter.params.provider.MethodSource;
 import de.dhbw.mh.slang.Value;
 import de.dhbw.mh.slang.ast.AstNode;
 
-@Disabled
 class JavaBytecodeGenerator8Test extends JavaBytecodeGeneratorTest {
-	
+
 	private static final int GROUP_ID = 8;
-	
-	
-	
+
+
+
 	/*==========================================*
 	 *                 LITERALS                 *
 	 *==========================================*/
-	
+
 	private static Stream<Arguments> literals() {
 		return allLiterals( )
 				.skip( GROUP_ID - 1 )
@@ -32,17 +31,17 @@ class JavaBytecodeGenerator8Test extends JavaBytecodeGeneratorTest {
 	void literalsSuccessfullyGenerated( Value value, String instruction ){
 		checkCodeGenerationForLiterals( value, instruction );
 	}
-	
+
 	/*==========================================*
 	 *            LOGICAL OPERATIONS            *
 	 *==========================================*/
-	
+
 	private static Stream<Arguments> logicalOperations() {
 		return allLogicalOperations( )
 				.skip( GROUP_ID - 7 )
 				.limit( 1 );
 	}
-	
+
 	@ParameterizedTest
 	@MethodSource("logicalOperations")
 	void logicalOperationsSuccessfullyGenerated( AstNode node, String a, String b, String c, String d, String e ){
