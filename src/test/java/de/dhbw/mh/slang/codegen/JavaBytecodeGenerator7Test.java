949e05c02617f2bc0fbfdba77b69786ddb23d685
marco-haupt <132950989+marco-haupt@users.noreply.github.com>
2023-06-01 22:49:06 +0200

    fix: assign an inferred type to logical operations

--- src/test/java/de/dhbw/mh/slang/codegen/JavaBytecodeGenerator7Test.java
+++ src/test/java/de/dhbw/mh/slang/codegen/JavaBytecodeGenerator7Test.java
@@ -2,11 +2,11 @@ package de.dhbw.mh.slang.codegen;
 
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import de.dhbw.mh.slang.Datatype;
 import de.dhbw.mh.slang.Value;
 import de.dhbw.mh.slang.ast.AstNode;
 
@@ -45,6 +45,7 @@ class JavaBytecodeGenerator7Test extends JavaBytecodeGeneratorTest {
 	@ParameterizedTest
 	@MethodSource("logicalOperations")
 	void logicalOperationsSuccessfullyGenerated( AstNode node, String a, String b, String c, String d, String e ){
+		node.setDatatype( Datatype.I32 );
 		checkCodeGenerationForLogicalOperations( node, a, b, c, d, e );
 	}
 
