3dd74747ad459c8b15ef3ac889820682863282c5
Silas <59054004+Tacconinja007@users.noreply.github.com>
2023-06-05 22:08:38 +0200

    Task solved
    All tests green

--- src/main/java/de/dhbw/mh/slang/antlr/AstBuilder.java
+++ src/main/java/de/dhbw/mh/slang/antlr/AstBuilder.java
@@ -10,15 +10,14 @@ import de.dhbw.mh.slang.antlr.SlangParser.LogicalOrExpressionContext;
 import de.dhbw.mh.slang.antlr.SlangParser.MultiplicativeExpressionContext;
 import de.dhbw.mh.slang.antlr.SlangParser.RelationalExpressionContext;
 import de.dhbw.mh.slang.antlr.SlangParser.SignedTermContext;
-import de.dhbw.mh.slang.ast.AstLiteral;
-import de.dhbw.mh.slang.ast.AstNode;
-import de.dhbw.mh.slang.ast.AstUnaryOperation;
+import de.dhbw.mh.slang.ast.*;
 import de.dhbw.mh.slang.craft.parser.CraftedSlangParser;
 import static de.dhbw.mh.slang.craft.Token.Type.MINUS;
 import static de.dhbw.mh.slang.craft.Token.Type.PLUS;
-import de.dhbw.mh.slang.ast.AstVariable;
+
 import de.dhbw.mh.slang.craft.lexer.NumericalEvaluator;
 import org.antlr.v4.runtime.tree.TerminalNode;
+import org.antlr.v4.runtime.tree.TerminalNodeImpl;
 
 public class AstBuilder extends SlangBaseVisitor<AstNode> {
 	
@@ -36,12 +35,33 @@ public class AstBuilder extends SlangBaseVisitor<AstNode> {
 	public AstNode visitLogicalAndExpression( LogicalAndExpressionContext ctx ){
 		return super.visitLogicalAndExpression( ctx );
 	}
-	
+
 	@Override
-	public AstNode visitEqualityExpression( EqualityExpressionContext ctx ){
-		return super.visitEqualityExpression( ctx );
+	public AstNode visitEqualityExpression(EqualityExpressionContext ctx) {
+		AstNode left = null;
+		for (int i = 0; i < ctx.children.size(); i++) {
+			if (i == 0) {
+				left = visitRelationalExpression(ctx.relationalExpression().get(i));
+			}
+
+			if (ctx.children.get(i) instanceof TerminalNodeImpl) {
+				String operator = ((TerminalNodeImpl) ctx.children.get(i)).getSymbol().getText();
+				switch (((TerminalNodeImpl) ctx.children.get(i)).getSymbol().getType()) {
+					case SlangLexer.EQ:
+						left = new AstBinaryOperation(null, left, AstBinaryOperation.Operator.COMPARE_EQUAL, visitRelationalExpression(ctx.relationalExpression(i + 1)));
+						break;
+					case SlangLexer.NE:
+						left = new AstBinaryOperation(null, left, AstBinaryOperation.Operator.COMPARE_UNEQUAL, visitRelationalExpression(ctx.relationalExpression(i + 1)));
+						break;
+					default:
+						throw new RuntimeException("Unknown operator");
+				}
+			}
+		}
+
+		return left;
 	}
-	
+
 	@Override
 	public AstNode visitRelationalExpression( RelationalExpressionContext ctx ){
 		return super.visitRelationalExpression( ctx );
